2f6a4707984759df278bcb602d0aa702ddc4d7ab
Mohammadreza Pasandideh <mohammadreza.pasandideh@amdocs.com>
2018-08-15 15:41:22 -0400

    Docker file: networkDiscovery serviceDecomposition
    Issue-ID: LOG-397 SDNC-317
    
    Change-Id: I80171ad4332e70872e5d3d8c42625f8669aa4d99
    Signed-off-by: Mohammadreza Pasandideh <mohammadreza.pasandideh@amdocs.com>

---
+++ pomba/network-discovery/src/main/docker/startService.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /opt/app
+if [ -z "${java_runtime_arguments}" ]; then
+    java -Dlogging.config=config/logback.xml -Xms128m -Xmx512m -jar /opt/app/lib/pomba-network-discovery.jar
+else
+    java -Dlogging.config=config/logback.xml $java_runtime_arguments -jar /opt/app/lib/pomba-network-discovery.jar
+fi
