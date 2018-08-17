27e8724988e5c31d2b56bf2a3f889415f2299c40
Mohammadreza Pasandideh <mohammadreza.pasandideh@amdocs.com>
2018-08-17 13:56:11 -0400

    Docker file: networkDiscovery serviceDecomposition
    Issue-ID: LOG-397 SDNC-317
    
    Change-Id: I0844eb12c8cd3b208f6a15cc16dd8717175bd5d8
    Signed-off-by: Mohammadreza Pasandideh <mohammadreza.pasandideh@amdocs.com>

--- pomba/network-discovery/src/main/docker/startService.sh
+++ pomba/network-discovery/src/main/docker/startService.sh
@@ -2,7 +2,7 @@
 
 cd /opt/app
 if [ -z "${java_runtime_arguments}" ]; then
-    java -Dlogging.config=config/logback.xml -Xms128m -Xmx512m -jar /opt/app/lib/pomba-network-discovery.jar
+    java -Dlogging.config=config/logback.xml -Xms128m -Xmx512m -jar /opt/app/lib/network-discovery.jar
 else
-    java -Dlogging.config=config/logback.xml $java_runtime_arguments -jar /opt/app/lib/pomba-network-discovery.jar
+    java -Dlogging.config=config/logback.xml $java_runtime_arguments -jar /opt/app/lib/network-discovery.jar
 fi
--- pomba/service-decomposition/src/main/docker/startService.sh
+++ pomba/service-decomposition/src/main/docker/startService.sh
@@ -2,7 +2,7 @@
 
 cd /opt/app
 if [ -z "${java_runtime_arguments}" ]; then
-    java -Dlogging.config=config/logback.xml -Xms128m -Xmx512m -jar /opt/app/lib/pomba-service-decomposition.jar
+    java -Dlogging.config=config/logback.xml -Xms128m -Xmx512m -jar /opt/app/lib/service-decomposition.jar
 else
-    java -Dlogging.config=config/logback.xml $java_runtime_arguments -jar /opt/app/lib/pomba-service-decomposition.jar
+    java -Dlogging.config=config/logback.xml $java_runtime_arguments -jar /opt/app/lib/service-decomposition.jar
 fi
